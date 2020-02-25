c45c2576c650d4e8add65b7f4b1e2f3fba9ddc5b
Buscems <46265895+Buscems@users.noreply.github.com>
2020-02-25 13:32:23 -0500

    yert

---
+++ RGBRPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //the following is in order to use rewired
+    [Tooltip("Reference for using rewired")]
+    [HideInInspector]
+    public Player myPlayer;
+    [Header("Rewired")]
+    [Tooltip("Number identifier for each player, must be above 0")]
+    public int playerNum;
+
+    enum Direction { North, South, East, West}
+    Direction currentDirection;
+    Vector2 direction;
+    bool isMoving;
+    Vector3 startPos, endPos;
+    float timeToMove;
+
+    private void Awake()
+    {
+        //Rewired Code
+        myPlayer = ReInput.players.GetPlayer(playerNum - 1);
+        ReInput.ControllerConnectedEvent += OnControllerConnected;
+        CheckController(myPlayer);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isMoving)
+        {
+            direction = new Vector2(myPlayer.GetAxis("MoveHorizontal"), myPlayer.GetAxis("MoveVertical"));
+
+            if(Mathf.Abs(direction.x) > direction.y)
+            {
+                direction.y = 0;
+            }
+            else
+            {
+                direction.x = 0;
+            }
+
+        }
+        
+
+
+    }
+
+    IEnumerator Movement()
+    {
+        isMoving = true;
+
+        endPos = new Vector3(startPos.x + System.Math.Sign(direction.x), startPos.y + System.Math.Sign(direction.y), startPos.z);
+
+        isMoving = false;
+        yield return 0;
+
+    }
+
+    //[REWIRED METHODS]
+    //these two methods are for ReWired, if any of you guys have any questions about it I can answer them, but you don't need to worry about this for working on the game - Buscemi
+    void OnControllerConnected(ControllerStatusChangedEventArgs arg)
+    {
+        CheckController(myPlayer);
+    }
+
+    void CheckController(Player player)
+    {
+        foreach (Joystick joyStick in player.controllers.Joysticks)
+        {
+            var ds4 = joyStick.GetExtension<DualShock4Extension>();
+            if (ds4 == null) continue;//skip this if not DualShock4
+            switch (playerNum)
+            {
+                case 4:
+                    ds4.SetLightColor(Color.yellow);
+                    break;
+                case 3:
+                    ds4.SetLightColor(Color.green);
+                    break;
+                case 2:
+                    ds4.SetLightColor(Color.blue);
+                    break;
+                case 1:
+                    ds4.SetLightColor(Color.red);
+                    break;
+                default:
+                    ds4.SetLightColor(Color.white);
+                    Debug.LogError("Player Num is 0, please change to a number > 0");
+                    break;
+            }
+        }
+    }
+
+}

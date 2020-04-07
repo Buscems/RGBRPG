cc1c09569407a27292417e5d5acd1730694da90f
Backsauce <gk.42@hotmail.com>
2020-04-07 16:10:02 -0400

    tried making a basic spawning script for the title scene but visualstudio was being real buggy and bad when i tried coding it so idk if it works or not but its in a folder i made and the logic is sound so idk man idk

---
+++ Assets/Scripts/Alex Scripts/TitleSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSpawner : MonoBehaviour
+{
+
+    public GameObject redGoop, greenGoop, blueGoop;
+    private int decider;
+    private float timer;
+    public float timerMin, timerMax;
+    public Vector3 spawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(timerMin, timerMax);
+        decider = Random.Range(0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.DeltaTime;
+
+        if(timer <= 0){
+            if(decider == 0){
+                Instantiate(redGoop, spawnPos, Quaternion.identity);
+            }
+            if (decider == 1)
+            {
+                Instantiate(greenGoop, spawnPos, Quaternion.identity);
+            }
+            if (decider == 2)
+            {
+                Instantiate(blueGoop, spawnPos, Quaternion.identity);
+            }
+            timer = Random.Range(timerMin, timerMax);
+            decider = Random.Range(0, 3);
+        }
+    }
+}
